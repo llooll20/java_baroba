3b2e90a2c2753436c3b86a56fa534aa4fc16c126
powerlth <133087517+powerlth@users.noreply.github.com>
2023-05-28 19:47:38 +0900

    Update TimetableView.java

--- TimetableView.java
+++ TimetableView.java
@@ -1,5 +1,3 @@
-package View;
-
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.*;
@@ -8,14 +6,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.Calendar;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAdjusters;
 public class TimetableView extends JPanel {
     public JTable timetable;
     int MonthOfToday; //현재 월 저장 변수
     int todayWeekOfDay; // 요일 주 저장 변수
+    int cnt = 22;
+
+    int number;
     public TimetableView() {
         setLayout(new BorderLayout());
         setTitlePanel();
@@ -32,68 +29,182 @@ public class TimetableView extends JPanel {
         Calendar c = Calendar.getInstance(); // 캘린더 인스턴스
         int currentDay = c.get(Calendar.DAY_OF_MONTH); // 현재날짜 가져오기
         todayWeekOfDay = (currentDay  - 1) / 7 + 1; // 요일 주 계싼
-        MonthOfToday = c.get(Calendar.MONTH); // 현재 월
+        MonthOfToday = c.get(Calendar.MONTH) + 1; // 현재 월        MonthOfToday =
         int numberOfWeeks = c.getActualMaximum(Calendar.WEEK_OF_MONTH); // MAX주
 
-        JLabel lblTitle = new JLabel((MonthOfToday + 1) + "월 " + todayWeekOfDay + "째주");
+        JLabel lblTitle = new JLabel(MonthOfToday + "월 " + todayWeekOfDay + "째주");//"?"월 "?"째주 - 
         lblTitle.setFont(new Font(DesignConstants.HANGUL_FONT, Font.BOLD, 25));
         lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
         lblTitle.setForeground(new Color(DesignConstants.SIGNATURE_COLOR));
-        titlePanel.add(lblTitle, BorderLayout.CENTER);
+        titlePanel.add(lblTitle, BorderLayout.CENTER);//TimetableView 정가운데 JLabel 생성 - 
         JButton lWeekBut;
         lWeekBut = new JButton("저번주");
         JButton nWeekBut;
         nWeekBut = new JButton("다음주");
-        class ListenForWeekButtons implements ActionListener { //버튼 클릭 이벤트 처리용 액션클래스
-            public void actionPerformed(ActionEvent e) {
-                if (e.getSource() == lWeekBut) {moveWeek(-1);} //저번주 버튼 클릭시 주 -1
-                else if (e.getSource() == nWeekBut) {moveWeek(1);} //다음주 버튼 클릭시 주 +1
-            }
-        }
-        ListenForWeekButtons lForWeekButtons = new ListenForWeekButtons();
-        lWeekBut.addActionListener(new ActionListener(){
+        lWeekBut.addActionListener(new ActionListener(){//lWeekBut 클릭 이벤트 발생 시 - 
             public void actionPerformed(ActionEvent e) {
                 todayWeekOfDay -= 1; //주 요일 감소
                 formattedFirstDayOfWeek -= 7;
+                cnt --;
+                System.out.println(cnt);
                 if (todayWeekOfDay == 0) { //주가 0이면 이전 달로 이동
-                    MonthOfToday -= 1;
-                    todayWeekOfDay = numberOfWeeks; // 주를 달의 최대 주로 설정
+                    if (cnt == 18){
+                        MonthOfToday = 4;
+                        todayWeekOfDay = 4;
+                        number = todayWeekOfDay;
+                    }
+                    if (cnt == 14){
+                        MonthOfToday = 3;
+                        todayWeekOfDay = 5;
+                        number = todayWeekOfDay;
+                    }
+                    if (cnt == 9){
+                        MonthOfToday = 2;
+                        todayWeekOfDay = 4;
+                        number = todayWeekOfDay;
+                    }
+                    if (cnt == 5){
+                        MonthOfToday = 1;
+                        todayWeekOfDay = 4;
+                        number = todayWeekOfDay;
+
+                    }
+                    if (cnt == 1){
+                        MonthOfToday = 12;
+                        todayWeekOfDay = 5;
+                        number = todayWeekOfDay;
+                        cnt = 52;
+                    }
+                    if (cnt == 47){
+                        MonthOfToday = 11;
+                        todayWeekOfDay = 4;
+                        number = todayWeekOfDay;
+                    }
+                    if (cnt == 43){
+                        MonthOfToday = 10;
+                        todayWeekOfDay = 4;
+                        number = todayWeekOfDay;
+                    }
+                    if (cnt == 39){
+                        MonthOfToday = 9;
+                        todayWeekOfDay = 4;
+                        number = todayWeekOfDay;
+                    }
+                    if (cnt == 35){
+                        MonthOfToday = 8;
+                        todayWeekOfDay = 5;
+                        number = todayWeekOfDay;
+                    }
+                    if (cnt == 30){
+                        MonthOfToday = 7;
+                        todayWeekOfDay = 4;
+                        number = todayWeekOfDay;
+                    }
+                    if (cnt == 26){
+                        MonthOfToday = 6;
+                        todayWeekOfDay = 5;
+                        number = todayWeekOfDay;
+                    }
+                    if (cnt == 22){
+                        MonthOfToday = 5;
+                        todayWeekOfDay = 5;
+                        number = todayWeekOfDay;
+                    }
                 }
+                if (MonthOfToday == 0) {
+                    MonthOfToday = 12;
+                }
+                lblTitle.setText(MonthOfToday + "월 " + todayWeekOfDay + "째주");//TitlePanel 다시 새기기 - 
+                drawTimetable();
             }
-});
-        nWeekBut.addActionListener(new ActionListener(){
+        });
+        nWeekBut.addActionListener(new ActionListener(){//nWeekBut 클릭 이벤트 발생 시 - 
             public void actionPerformed(ActionEvent e) {
                 todayWeekOfDay += 1; //주 요일 증가
                 formattedFirstDayOfWeek += 7;
-                if (todayWeekOfDay == numberOfWeeks) { //주가 0이면 이전 달로 이동
-                    MonthOfToday += 1;
-                    todayWeekOfDay = 1; // 주를 달의 첫주로 설정
+                cnt++;
+                System.out.println(number);
+                if (MonthOfToday == 4) { //주가 0이면 이전 달로 이동
+                    if (todayWeekOfDay == 5) {
+                        MonthOfToday = 5;
+                        todayWeekOfDay = 1;
+                    }
                 }
+                if (MonthOfToday == 3) {
+                    if (todayWeekOfDay == 6){
+                        MonthOfToday = 4;
+                        todayWeekOfDay = 1;
+                    }
+                }
+                if (MonthOfToday == 2) {
+                    if (todayWeekOfDay == 5){
+                        MonthOfToday = 3;
+                        todayWeekOfDay = 1;
+                    }
+                }
+                if (MonthOfToday == 1) {
+                    if (todayWeekOfDay == 5){
+                        MonthOfToday = 2;
+                        todayWeekOfDay = 1;
+                    }
+                }
+                if (MonthOfToday == 12) {
+                    if (todayWeekOfDay == 6){
+                        MonthOfToday = 1;
+                        todayWeekOfDay = 1;
+                        cnt = 0;
+                    }
+                }
+                if (MonthOfToday == 11) {
+                    if (todayWeekOfDay == 6){
+                        MonthOfToday = 12;
+                        todayWeekOfDay = 1;
+                    }
+                }
+                if (MonthOfToday == 10) {
+                    if (todayWeekOfDay == 5){
+                        MonthOfToday = 11;
+                        todayWeekOfDay = 1;
+                    }
+                }
+                if (MonthOfToday == 9) {
+                    if (todayWeekOfDay == 5){
+                        MonthOfToday = 10;
+                        todayWeekOfDay = 1;
+                    }
+                }
+                if (MonthOfToday == 8) {
+                    if (todayWeekOfDay == 6){
+                        MonthOfToday = 9;
+                        todayWeekOfDay = 1;
+                    }
+                }
+                if (MonthOfToday == 7) {
+                    if (todayWeekOfDay == 5){
+                        MonthOfToday = 8;
+                        todayWeekOfDay = 1;
+                    }
+                }
+                if (MonthOfToday == 6) {
+                    if (todayWeekOfDay == 6){
+                        MonthOfToday = 7;
+                        todayWeekOfDay = 1;
+                    }
+                }
+                if (MonthOfToday == 5) {
+                    if (todayWeekOfDay == 6){
+                        MonthOfToday = 6;
+                        todayWeekOfDay = 1;
+                    }
+                }
+
+                lblTitle.setText(MonthOfToday + "월 " + todayWeekOfDay + "째주");//TitlePanel 다시 새기기 - 
+                drawTimetable();
             }
         });
         titlePanel.add(lWeekBut, BorderLayout.WEST);
-        nWeekBut.addActionListener(lForWeekButtons);
         titlePanel.add(nWeekBut, BorderLayout.EAST);
         add(titlePanel, BorderLayout.NORTH); // 메인 패널(this) 상단에 titlePanel 추가
-    }
-    public int moveWeek(int week){ //임시로 만든 메소드 해결되면 지울것
-        Calendar c = Calendar.getInstance();
-        int firstDayOfWeek = c.get(Calendar.DAY_OF_WEEK);
-        int currentDay = c.get(Calendar.DAY_OF_MONTH);
-        int todayWeekOfDay = (currentDay + firstDayOfWeek - 2) / 7 + 1;
-        int MonthOfToday = c.get(Calendar.MONTH);
-        int numberOfWeeks = c.getActualMaximum(Calendar.WEEK_OF_MONTH); // MAX주
-        todayWeekOfDay += week;
-        if (todayWeekOfDay >= numberOfWeeks) while (todayWeekOfDay >= numberOfWeeks) {
-            MonthOfToday++;
-            todayWeekOfDay = 1;
-        }
-        else if (todayWeekOfDay < 0) while (todayWeekOfDay < 0) {
-            MonthOfToday--;
-            todayWeekOfDay = numberOfWeeks;
-
-        }
-        return todayWeekOfDay;
     }
     private void setTimetablePanel() { // 시간표 표 짜기
         String[] columnNames = {"","월","화","수","목","금","토","일"}; // 첫 행 정하기
@@ -177,15 +288,23 @@ public class TimetableView extends JPanel {
     } // customCellRenderer Class
     TimetableView timetableView; // 클래스 인스턴스 선언
     int formattedFirstDayOfWeek; //주의 첫번째 요일 선언
-    public void drawTimetable() {
-        for (int i = 0; i == 7; i++){
-            letthisweek();
+    public void drawTimetable() {//문제 발생 - 
+        formattedFirstDayOfWeek = Get_Monday_of_Week(MonthOfToday-1, todayWeekOfDay);//현재 MonthOfToday, todayWeekOfDay의 월요일 날짜 반환 - 
+        for (int i = 0; i < 7; i++){
+            //letthisweek();
             int FDOW = (formattedFirstDayOfWeek+i); //주의 첫요일 + i, 0~7이므로 월화수목금토일까지 돌아감
             boolean result = isExist(String.valueOf(FDOW)); //파일이 존재하면 1, 없으면 0
+
             if (result) {//존재하면 년월을 제외하고 뒷부분을 fileTime에 담고, fileTime 시간만큼 fillTimetableCell을 통해 시간표 차지
                 File directory = new File("MemoData/");
-                String[] fileNames = directory.list();
-                String fileTime = fileNames[0].substring(8);
+                File[] files = directory.listFiles();
+                String fileTime = null;
+                for(File f : files){
+                    if(f.getName().substring(0,8).equals(String.valueOf(FDOW))){
+                        fileTime = f.getName().substring(8, 19);
+                    }
+                }
+                JOptionPane.showMessageDialog(null, "drawTimetable\ni : " + i + ", fileTime : " + fileTime);
                 fillTimetableCell(String.valueOf(i), fileTime);
             }
         }
@@ -236,19 +355,27 @@ public class TimetableView extends JPanel {
         }
         table.setValueAt(memoAreaText, startRowIndex+2,columnIndex);
     }
-    public int letthisweek(){//파일이름에서 년월일 추출하는 메소드
-        LocalDate currentDate = LocalDate.now(); //오늘날짜 받아오기
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd"); //20230521 이런식의 포맷으로 받아올것
-        LocalDate firstDayOfWeek = currentDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)); //오늘날짜 주의 월요일을 받아옴
-        int formattedFirstDayOfWeek = Integer.parseInt(firstDayOfWeek.format(formatter)); //오늘날짜 주의 월요일을 위의 포맷으로 받음
-        return formattedFirstDayOfWeek;
-    }//주의 첫 일 구하기
     private boolean isExist(String DOW){ //파일이 존재하는지 확인하는 메소드, DOW는 formattedFirstDayOfWeek과 같음
         File directory = new File("MemoData/");
-        String[] fileNames = directory.list(); //폴더안 파일은 하나밖에 없으므로 리스트로 받아와도됨
-        String fileDate = fileNames[0].substring(0,8); // 파일의 년원일부분 추출
-        return fileDate == DOW;
+        
+        File[] files = directory.listFiles();
+        boolean isFileExist = false;
+        for(File f : files){
+            if(f.getName().substring(0,8).equals(DOW))isFileExist = true;
+        }
+        return isFileExist;
     }//날짜와 일치하는 파일 존재하는지 확인
+    private int Get_Monday_of_Week(int Month, int DayOfWeek) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2023);
+        calendar.set(Calendar.MONTH, Month);
+        calendar.set(Calendar.WEEK_OF_MONTH, DayOfWeek);
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+        int monday_year = calendar.get(Calendar.YEAR) * 10000;
+        int monday_month = (calendar.get(Calendar.MONTH) + 1) * 100;
+        int monday_day = calendar.get(Calendar.DATE);
+        return monday_year + monday_month + monday_day;
+    }
 
     public static void main(String args[]){ //안돼서 해보는 main 메소드, 일단 남겨두지만 지울것
         SwingUtilities.invokeLater(new Runnable(){
@@ -258,7 +385,7 @@ public class TimetableView extends JPanel {
         });
     }
 } // TimetableView Class
- class DesignConstants {
+class DesignConstants {
     public static final int SIGNATURE_COLOR = 0x334D61;
     public static final int TIMETABLE_CELL_COLOR = 0xFFF7CB;
     public static final String HANGUL_FONT = "Malgun Gothic";
